e2d44a341d6f147123902b983026fe424592edfd
Eloy Castelo <e.castelo.otero@gmail.com>
2022-06-04 18:30:52 +0200

    refactor: get rid of dots blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,48 +1,4 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   site: 'https://eloy.co',
-  markdown: {
-    render: [
-      '@astrojs/markdown-remark',
-      {
-        rehypePlugins: [
-          'rehype-slug',
-          [
-            'rehype-autolink-headings',
-            {
-              behavior: 'prepend',
-              content: {
-                type: 'element',
-                tagName: 'span',
-                properties: { className: ['heading-link'] },
-                children: [
-                  {
-                    type: 'element',
-                    tagName: 'img',
-                    properties: { src: '/assets/icons/link.svg' },
-                    children: [],
-                  },
-                ],
-              },
-            },
-          ],
-          [
-            'rehype-external-links',
-            {
-              content: {
-                type: 'element',
-                tagName: 'img',
-                properties: {
-                  src: '/assets/icons/external-link.svg',
-                  alt: 'External link icon',
-                },
-                children: [],
-              },
-              contentProperties: { className: ['external-link-icon'] },
-            },
-          ],
-        ],
-      },
-    ],
-  },
 });
