85baacf2b2f78e356c2c912ef47eaca1840c4a16
Eloy Castelo <e.castelo.otero@gmail.com>
2022-07-09 12:52:18 +0200

    chore: add robots.txt and sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://eloy.co/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   vite: {
     ssr: {
       external: ['@11ty/eleventy-img', 'svgo'],
